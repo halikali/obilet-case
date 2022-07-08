8f8cc334863b683b6d975f0e3fca1838475f2dbe
halikali <tunchalikali@gmail.com>
2022-07-08 13:24:28 +0300

    complete city input component

--- src/components/Inputs/CityInput/CityInput.component.jsx
+++ src/components/Inputs/CityInput/CityInput.component.jsx
@@ -1,14 +1,47 @@
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { setdestination, setOrigin } from "Slices/FormSlices";
 import { Location } from "components/Icons/Icons.component";
+import Dropdown from "components/Dropdown/Dropdown.component";
 import "./CityInput.style.scss";
 
-const CityInput = ({ label, city }) => {
+const CityInput = ({ label, city, inputName }) => {
+  const [cityName, setCityName] = useState(city);
+  const dispatch = useDispatch();
+
+  const { origin, destination } = useSelector((state) => state.form.data);
+
+  useEffect(() => {
+    inputName === "origin"
+      ? dispatch(setOrigin({ ...origin, name: cityName }))
+      : dispatch(setdestination({ ...destination, name: cityName }));
+  }, [cityName]);
+
+  useEffect(() => {
+    inputName === "origin"
+      ? setCityName(origin.name)
+      : setCityName(destination.name);
+  }, [origin, destination]);
+
+  const onchangeHandler = (e) => {
+    setCityName(e.target.value);
+  };
+
   return (
     <div className="selection">
       <Location className="selection__icon" />
       <div className="selection__text-wrapper">
         <p className="label">{label}</p>
-        <p className="text"> {city}</p>
+        <input
+          className="text input"
+          placeholder="il veya ilçe adı yazın "
+          type={"text"}
+          onChange={(e) => onchangeHandler(e)}
+          value={cityName}
+        />
       </div>
+      <Dropdown seperator={inputName} />
     </div>
   );
 };
