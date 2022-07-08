84d855fbd089e9d5e71ac37b220a14b0c8cbda1f
halikali <tunchalikali@gmail.com>
2022-07-08 13:25:38 +0300

    complete homepage

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,23 +1,50 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 import CityInput from "components/Inputs/CityInput/CityInput.component";
 import DateInput from "components/Inputs/DateInput/DateInput.component";
+import { fetchJourney, fetchLocations } from "Slices/JourneySlice";
+import { changeValues } from "Slices/FormSlices";
 import { Switch } from "components/Icons/Icons.component";
 import "./HomePage.style.scss";
 
 const HomePage = () => {
+  const dispatch = useDispatch();
+  const { origin, destination, departureDate } = useSelector(
+    (state) => state.form.data
+  );
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/startSession")
+      .then(() => dispatch(fetchLocations()));
+  }, []);
+
+  const switchInputValues = () => {
+    dispatch(changeValues());
+  };
+
+  const findJourney = () => {
+    dispatch(fetchJourney({ origin, destination, departureDate }));
+  };
+
   return (
     <div className="homepage">
       <div className="header"></div>
       <div className="content">
         <div className="selection__city-areas-wrapper">
-          <CityInput label={"Nereden"} city="istanbul" />
-          <CityInput label={"Nereye"} city="Ankara" />
-          <Switch className="icon" />
+          <CityInput label={"Nereden"} city="İzmir" inputName={"origin"} />
+          <CityInput label={"Nereye"} city="Ankara" inputName={"destination"} />
+          <Switch className="icon" onClick={() => switchInputValues()} />
         </div>
-
         <DateInput />
-        <Link to={"/journey-list"} className="find-button">
+        <Link
+          to={"/journey-list"}
+          className="find-button"
+          onClick={findJourney}
+        >
           Bileti Bul
         </Link>
       </div>
