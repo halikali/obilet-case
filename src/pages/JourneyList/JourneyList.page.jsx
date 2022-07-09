51d9d74973896bedd4664f41200aaa43ceb7274d
halikali <tunchalikali@gmail.com>
2022-07-09 13:28:50 +0300

    error management for journey lis page

--- src/pages/JourneyList/JourneyList.page.jsx
+++ src/pages/JourneyList/JourneyList.page.jsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import JourneyCard from "components/JourneyCard/JourneyCard.component";
 import { ArrowBack } from "components/Icons/Icons.component";
 import { formatDay } from "helper";
+import { JourneyCardSkeleton } from "components/Skeleton/Skeleton.component";
 import "./JourneyListPage.style.scss";
 
 const JourneyListPage = () => {
@@ -17,6 +18,48 @@ const JourneyListPage = () => {
   const goBack = () => {
     navigate(-1);
   };
+
+  if (!journey.isLoad) {
+    return (
+      <div className="journey-list-page">
+        <div className="header">
+          <ArrowBack className="header__icon" onClick={() => goBack()} />
+          <div className="header__info-wrapper">
+            <p className="header__info">
+              {origin.name} - {destination.name}
+            </p>
+            <p className="header__info header__info--date">
+              {formatDay(departureDate)}
+            </p>
+          </div>
+        </div>
+        {Array.from({ length: 10 }).map((_, i) => (
+          <JourneyCardSkeleton key={i} />
+        ))}
+      </div>
+    );
+  }
+
+  if (journey.isLoad && Array.from(journey.data.data).length === 0) {
+    return (
+      <div className="journey-list-page">
+        <div className="header">
+          <ArrowBack className="header__icon" onClick={() => goBack()} />
+          <div className="header__info-wrapper">
+            <p className="header__info">
+              {origin.name} - {destination.name}
+            </p>
+            <p className="header__info header__info--date">
+              {formatDay(departureDate)}
+            </p>
+          </div>
+        </div>
+        <p className="info-message">
+          Belirtilen tarihler arasında bir sefer kaydı bulunamamıştır.
+        </p>
+      </div>
+    );
+  }
 
   return (
     <div className="journey-list-page">
@@ -32,7 +75,7 @@ const JourneyListPage = () => {
         </div>
       </div>
       {journey.data.status !== "Success" ? (
-        <div> {journey.data["user-message"]} </div>
+        <p className="info-message"> {journey.data["user-message"]} </p>
       ) : (
         Array.from(journey.data.data).map((item) => (
           <JourneyCard
