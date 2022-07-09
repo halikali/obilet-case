b251915c6ffdb5aa34beddd8624d5ed59b649a8f
halikali <tunchalikali@gmail.com>
2022-07-09 14:55:46 +0300

    pass the CORS barrier

--- src/backend.js
+++ src/backend.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(cors());
 
 let sessionInfo = {
   deviceId: "",
