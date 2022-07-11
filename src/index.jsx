b59a8820d0b4a9cf34e07df89dd6eab82773aa7b
halikali <tunchalikali@gmail.com>
2022-07-11 14:13:08 +0300

    refactor: import reset css to index css file

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 
 import { store } from "store";
 import App from "./App";
-import "./CommonCSS/_reset.scss";
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
